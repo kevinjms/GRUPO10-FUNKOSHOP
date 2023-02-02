83928fedf56eac95e5fb54b631d2ac2dfaa4e4c6
Cami09111 <121905232+Cami09111@users.noreply.github.com>
2023-02-01 19:11:42 -0500

    carrito de compras

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-const publicPath= path.resolve(__dirname, 'public')
-app.use(express.static(publicPath));
 app.use(express.static('public'));
+
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname,'/views/index.html'))
+})
+
+app.get('/carrito-de-compras', (req, res) =>{
+    res.sendFile(path.join(__dirname,'/views/carrito-de-compras.html'))
+})
+
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`server started on http://localhost:${port}`);
