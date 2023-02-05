cd44dd73766989732b1781392e267ad31b814e2f
valentino <arztvalentino@gmail.com>
2023-02-05 03:54:11 -0300

    formulario de login

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.get('/carrito-de-compras', (req, res) =>{
     res.sendFile(path.join(__dirname,'/views/carrito-de-compras.html'))
 })
 
+app.get('/formulario-de-login', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/formulario-de-login.html'))
+})
 
 const port = 3000;
 app.listen(port, () => {
