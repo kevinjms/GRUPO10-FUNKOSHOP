afe6236083ad35a99f9ae44ca43be8b54c3577e3
Micaela Berto <micaela@MBP-de-Micaela.fibertel.com.ar>
2023-02-06 16:19:31 -0300

    app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname,'/views/index.html'))
-})
+});
 
 app.get('/carrito-de-compras', (req, res) =>{
     res.sendFile(path.join(__dirname,'/views/carrito-de-compras.html'))
@@ -20,7 +20,6 @@ app.get('/formulario-de-registro', (req, res) => {
 app.get('/formulario-de-login', (req, res) => {
     res.sendFile(path.join(__dirname,'/views/formulario-de-login.html'))
 });
-
 
 const port = 3000;
 app.listen(port, () => {
