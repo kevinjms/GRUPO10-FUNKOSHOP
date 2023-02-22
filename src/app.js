5ebfb03555a2e7ec96991f75254d533fbb996cef
Micaela Berto <micaela@MBP-de-Micaela.fibertel.com.ar>
2023-02-22 20:10:08 -0300

    app

--- src/app.js
+++ src/app.js
@@ -12,33 +12,6 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', mainRoutes);
 
-// app.get('/', (req, res) =>{
-//     res.render(path.join(__dirname,'index'))
-// });
-
-// app.get('/detalle-de-producto', (req, res) =>{
-//     res.render(path.join(__dirname,'detalle-de-producto'))
-// });
-
-// app.get('/carrito-de-compras', (req, res) =>{
-//     res.render(path.join(__dirname,'carrito-de-compras'))
-// });
-
-// app.get('/formulario-de-registro', (req, res) => {
-//     res.sendFile(path.join(__dirname,'/views/register.html'))
-// });
-
-// app.post('/formulario-de-registro', (req, res) => {
-//     res.redirect('/')
-// })
-
-// app.get('/formulario-de-login', (req, res) => {
-//     res.sendFile(path.join(__dirname,'/views/formulario-de-login.html'))
-// });
-
-// app.post('/formulario-de-login', (req, res) => {
-//     res.redirect('/')
-// })
 
 const port = 3000;
 app.listen(port, () => {
