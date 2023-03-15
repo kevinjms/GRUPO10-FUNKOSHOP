9cb63e5f75fb3e912c6e07e126a0207a599fe126
valentino <arztvalentino@gmail.com>
2023-03-14 23:46:02 -0300

    crud2

--- src/routers/main.js
+++ src/routers/main.js
@@ -8,8 +8,5 @@ router.get('/', mainController.index);
 router.get('/carrito-de-compras', mainController.car)
 router.get('/formulario-de-login', mainController.login)
 router.get('/register', mainController.register)
-router.get('/detalle-de-producto', mainController.detail)
-router.get('/creacion-de-productos', mainController.create)
-router.get('/edicion-de-productos', mainController.edit)
 
 module.exports = router;
