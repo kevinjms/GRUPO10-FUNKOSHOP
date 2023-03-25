52b35d2fef61f7f091e588f54ed8cd3fd31cf281
valentino <arztvalentino@gmail.com>
2023-03-24 21:01:28 -0300

    rutas

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,10 +1,5 @@
 const express = require('express');
 
-<<<<<<< HEAD
-=======
-const fs = require ('fs');
-
->>>>>>> b35a2c6e8768d7d94b926ecde4330a2743dc80bc
 const productsController = require('../controllers/productsControllers');
 
 const router = express.Router();
@@ -12,8 +7,8 @@ const router = express.Router();
 router.get('/listado-de-productos', productsController.list)
 router.get('/create/creacion-de-productos', productsController.create)
 router.get('/:id/detalle-de-producto', productsController.detail)
-// router.post('/listado-de-productos', productsController)
+router.post('/listado-de-productos', productsController.list)
 router.get('/:id/edit/edicion-de-productos', productsController.edit)
-// router.put('/:id/listado-de-productos', productsController)
-// router.get('/:id', productsController.delete)
+router.put('/:id/listado-de-productos', productsController.list)
+router.get('/:id', productsController.delete)
 module.exports = router;
