ebbe07f9aa308e2910701e0508237749d30bf964
kevinjms <kevinjoelms@gmail.com>
2023-03-26 17:09:41 -0300

    .

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 
 const productsFilePath = path.join(__dirname, '../public/data/products.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+function getProducts() {
+    return JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+};
 
 
 const controller = {
@@ -16,8 +18,10 @@ const controller = {
     edit: (req, res) => {
         res.render('edicion-de-productos');
     },
-    list: (req, res) => {
-        res.render('listado-de-productos');
+    list:  (req, res) => {
+        const products = getProducts();
+        res.render('listado-de-productos',
+        {products});
     },
     delete: (req, res) => {
         res.render();
