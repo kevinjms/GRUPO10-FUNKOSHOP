cd934004edf50c7e3b54919db52bf2fe1deebb74
Micaela Berto <micaela@MacBook-Pro-de-Micaela.local>
2023-03-29 15:35:22 -0300

    m

--- src/routers/products.js
+++ src/routers/products.js
@@ -9,15 +9,15 @@ router.get('/', productsController.list);
 
 router.get('/detail/:id', productsController.detail);
 
- router.get('/create/creacion-de-productos', productsController.create);
+router.get('/create/creacion-de-productos', productsController.create);
 
- router.post('/listado-de-productos', productsController.list);
+router.post('/listado-de-productos', productsController.list);
 
- router.get('/:id/edit/edicion-de-productos', productsController.edit);
+router.get('/:id/edit/edicion-de-productos', productsController.edit);
 
- router.put('/:id/listado-de-productos', productsController.list);
+router.put('/:id/listado-de-productos', productsController.list);
 
- router.get('/:id', productsController.delete);
+router.get('/:id', productsController.delete);
 
 
-module.exports = router
+module.exports = router;
