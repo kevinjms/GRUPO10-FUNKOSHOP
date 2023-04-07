c49267f748a68203cd4bf158ae3da0a91f20be78
valentino <arztvalentino@gmail.com>
2023-04-06 22:52:40 -0300

    registro2

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -5,7 +5,7 @@ const usersController = require('../controllers/usersControllers');
 const uploadFile = require('../middlewares/usersMiddleware');
 
 router.get('/register', usersController.register);
-router.post('/register', uploadFile.single("image"), usersController.registered)
+router.post('/', uploadFile.single("image"), usersController.registered)
 router.get('/login', usersController.login);
 
 module.exports = router
