4e74e1a68284815e64d12c6741d4b392ccb3b0be
kevinjms <kevinjoelms@gmail.com>
2023-06-09 19:55:09 -0300

    index

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const db = require('../database/models')
-const {Op} = require('sequelize')
+const { Op } = require('sequelize')
 
 const productsFilePath = path.join(__dirname, '../data/products.json');
 function getProducts() {
@@ -9,21 +9,37 @@ function getProducts() {
 };
 
 const controller = {
-    index: (req, res) => {
-        const products = getProducts();
-        const visited = products.filter((product) => product.subcategory == 'visited');
-        const inSale = products.filter((product) => product.subcategory == 'in-sale');
-        res.render('index', { visited, inSale });
-    },
+    index: async (req, res) => {
+        try {
+          const visited = await db.Product.findAll({
+            include: [{ association: "subcategories" }, { model: db.Image, as: 'images' }],
+            where: {
+              product_subcategories_id: 2
+            }
+          });
+      
+          const inSale = await db.Product.findAll({
+            include: [{ association: "subcategories" }, { model: db.Image, as: 'images' }],
+            where: {
+              product_subcategories_id: 1
+            }
+          });
+      
+          res.render('index', { inSale, visited });
+        } catch (error) {
+          console.error(error);
+          res.status(500).send('Error guachinnn');
+        }
+      },
     car: (req, res) => {
         const products = getProducts();
         const visited = products.filter((product) => product.subcategory == 'visited');
         res.render('carrito-de-compras', { visited });
-    }, 
-    us: (req, res)  => {
+    },
+    us: (req, res) => {
         res.render('aboutUs');
     },
-    personalized: (req, res)  => {
+    personalized: (req, res) => {
         res.render('personalized');
     }
 }
