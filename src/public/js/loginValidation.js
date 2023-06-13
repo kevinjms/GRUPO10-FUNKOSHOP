97568a07e80c7cafbdbc93b3ccc7ae62a0e4e225
kevinjms <kevinjoelms@gmail.com>
2023-06-13 14:06:34 -0300

    boton aparecer/ocultar contraseña

--- src/public/js/loginValidation.js
+++ src/public/js/loginValidation.js
@@ -1,16 +1,16 @@
 window.onload = function () {
-    const form = document.querySelector("form");
-    form.email.focus()
+  const form = document.querySelector("form");
+  form.email.focus()
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-console.log(form)
-        const errorsElement = document.querySelectorAll(".text-danger");
-        errorsElement.forEach(element => {
-            element.style.display = "none"
-            element.innerHTML = "";
-        })
-        const errors = [];
+    console.log(form)
+    const errorsElement = document.querySelectorAll(".text-danger");
+    errorsElement.forEach(element => {
+      element.style.display = "none"
+      element.innerHTML = "";
+    })
+    const errors = [];
 
     // Email
     if (!form.email.value) {
@@ -46,27 +46,44 @@ console.log(form)
       });
       form.password.classList.remove("is-valid");
       form.password.classList.add("is-invalid");
-  } else if (form.password.value.length < 8) {
+    } else if (form.password.value.length < 8) {
       errors.push({
         name: "password",
         message: "La contraseña debe tener minimo 8 caracteres",
       });
       form.password.classList.remove("is-valid");
       form.password.classList.add("is-invalid");
-  } else {
+    } else {
       form.password.classList.remove("is-invalid");
       form.password.classList.add("is-valid");
     }
 
-   //Error campos vacios
+    //Error campos vacios
     errors.forEach((error) => {
       console.log(error)
       const errorLabel = document.getElementById("error-" + error.name);
       errorLabel.style.display = "block"
       errorLabel.innerHTML = error.message;
     })
-    if (errors.length === 0){
+    if (errors.length === 0) {
       form.submit();
     }
   });
 }
+
+const togglePasswordButton = document.getElementById('toggle-password');
+const passwordInput = document.querySelector('input[name="password"]');
+const passwordIcon = document.querySelector('.fa-solid.fa-eye');
+
+togglePasswordButton.addEventListener('click', function () {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    passwordIcon.classList.remove('fa-eye');
+    passwordIcon.classList.add('fa-eye-slash');
+  } else {
+    passwordInput.type = 'password';
+    passwordIcon.classList.remove('fa-eye-slash');
+    passwordIcon.classList.add('fa-eye');
+  }
+});
+
