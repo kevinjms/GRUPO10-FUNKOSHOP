1b8f773b10ec6fcb5a3cbd58a93a7594d07f4837
valentino <arztvalentino@gmail.com>
2023-06-16 01:04:00 -0300

    image

--- src/middlewares/cookieMiddleware.js
+++ src/middlewares/cookieMiddleware.js
@@ -1,9 +1,7 @@
 const db = require('../database/models')
 
 function recordameMiddleware(req, res, next) {
-    console.log(req.session)
     if (req.cookies.recordame != undefined && req.session.user == undefined) {
-        console.log('hola')
         const userCookie = ''
         const users = db.User.findAll()
         for (let i = 0; i < users.length; i++) {
